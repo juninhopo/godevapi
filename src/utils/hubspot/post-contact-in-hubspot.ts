a3f73d9766377920cd704205be0fc09706a6e205
Darlan Jr <kelgaxd@gmail.com>
2024-10-02 01:53:45 -0400

    feat: create function to post contact in hubspot

---
+++ src/utils/hubspot/post-contact-in-hubspot.ts
@@ -0,0 +1,36 @@
+import axios from "axios"
+import { Contact } from "./create-contact-in-hubspot"
+
+const putContactInHubSpot = async (data: Contact) => {
+  try {
+    console.log({
+      message: 'Uploading contact to HubSpot',
+      data,
+    })
+
+    await axios.post(
+      `${process.env.HUBSPOT_BASE_URL}/crm/v3/objects/contacts`,
+      {
+        "properties": {
+          email: data.email,
+          firstname: data.name,
+          phone: data.phone,
+          company: data.company,
+          website: data.site,
+        },
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${process.env.HUBSPOT_API_KEY}`,
+        }
+      },
+    )
+  } catch (error: any) {
+    console.error({
+      message: 'Error on put contact in HubSpot',
+      error,
+    })
+  }
+}
+
+export default putContactInHubSpot
