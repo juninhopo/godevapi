a6a6322fc98531ace52140dae213d90f021798e7
Darlan Jr <kelgaxd@gmail.com>
2024-10-02 01:54:56 -0400

    feat: create function to use with use-case for create a new contact in hubspot

---
+++ src/utils/hubspot/create-contact-in-hubspot.ts
@@ -0,0 +1,33 @@
+import validateAlreadyContact from "./validate-already-contact"
+import validateCorporativeEmail from "../validate-corporative-email"
+import putContactInHubSpot from "./post-contact-in-hubspot"
+
+export type Contact = {
+  company: string
+  name: string
+  email: string
+  phone: string
+  site: string
+}
+
+const createContactInHubSpot = async (data: Contact) => {
+  // Validate if contact is corporative email
+  validateCorporativeEmail(data.email)
+
+  // Validate if contact is already in HubSpot 
+  const is_contact = await validateAlreadyContact(data.email)
+
+  // Upload contact to HubSpot
+  if (!is_contact) {
+    await putContactInHubSpot(data)
+  } else {
+    console.log({
+      message: 'Contact already in HubSpot',
+      data,
+    })
+
+    return
+  }
+}
+
+export default createContactInHubSpot
